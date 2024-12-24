5a6700f110250e3da0ee35973e3dcbf0fb8294ba
Z <155460219+z-jinke@users.noreply.github.com>
2024-12-25 03:12:33 +0800

    删除

--- Script/safari.js
+++
@@ -1,8 +0,0 @@
-let url = $request.url;
-
-if (/www\.google\.cn/.test(url)) {
-
-    url = url.replace("www.google.cn", "www.google.com");
-}
-
-$done({ response: { status: 302, headers: { Location: url } } });
